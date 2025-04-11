36c2e3be385740531ff024cd274641dcfa61d064
nivasnick <141710560+nivasnick@users.noreply.github.com>
2025-04-12 00:00:24 +0530

    Create test_dummy.py

---
+++ tests/test_dummy.py
@@ -0,0 +1,6 @@
+platform linux -- Python 3.10.16, pytest-8.3.4, pluggy-1.5.0
+rootdir: /home/runner/work/nifty50-portfolio-builder/nifty50-portfolio-builder
+collected 0 items
+
+============================ no tests ran in 0.00s =============================
+Error: Process completed with exit code 5.
